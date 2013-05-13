f2544eff21c639b8079c73b6d64eb9e7296d1fdc
Speedy Gonzales <vahid.hedayati@gmail.com>
2013-05-13 13:43:26 +0200

    Update working-web.sh

--- working-web.sh
+++ working-web.sh
@@ -6,32 +6,65 @@
 url=$1;
 pattern=$2;
 
-if [[ $url  =~ http:// ]]; then
-        h_url=$url
-       	c_url=$(echo $url|sed -e "s:http\://::g")
-elif [[ $url  =~ https:// ]]; then
-  	h_url=$url
-        c_url=$(echo $url|sed -e "s:https\://::g")
 
+
+PORT=80;
+
+if [[ $url  =~ http:// ]]; then
+       	c_url=$(echo $url|sed -e "s:http\://::g")
+elif [[ $url =~ https:// ]]; then
+        c_url=$(echo $url|sed -e "s:https\://::g")
+	PORT=443;
 else
-	h_url="http://"$url;
 	c_url=$url;
 fi
+
+ if [[ $c_url =~ : ]]; then
+	C_PORT=${c_url##*:}
+	if [[ $C_PORT =~ / ]]; then
+	 C_PORT=${C_PORT%%/*} 
+	fi
+ 	PORT=$C_PORT
+ fi
+
+	if [[ $c_url =~ / ]]; then
+		b_url=${c_url%%/*}
+  		e_url=${c_url#*/}
+		if [ "$e_url" == "" ]; then
+			e_url="/";
+		else
+			e_url="/"$e_url;
+		fi
+		c_url=$b_url
+	else
+		b_url=$c_url;
+		e_url="/";
+	fi
+	if [[ $PORT =~ 443 ]]; then 
+	h_url="https://"$b_url":"$PORT$e_url
+	else
+	h_url="http://"$b_url":"$PORT$e_url
+	fi
+	#echo $h_url
 s=$(date +%s.%N)
 elinks --dump "$h_url" | grep "$pattern" >/dev/null 2>&1
 if [ $? -ne 0 ] ; then
 	f=$(date +%s.%N)
 	sum=$(echo $f|awk -v s=$s '{$3 = $1 - 's';  printf "%f", $3}')
-	echo "SESSION CONTENT CRITICAL: URL $h_url is down or content $pattern was not found on the webpage|time="$sum"s;;;;0 size=0B;;;0"
+	echo "CONTENT_CHECK CRITICAL: URL $h_url is down or content $pattern was not found on the webpage|time="$sum"s;;;;0 size=0B;;;0"
 	exit 2
 else
 	f=$(date +%s.%N)
 	sum=$(echo $f|awk -v s=$s '{$3 = $1 - 's';  printf "%f", $3}')
-	head="POST $h_url HTTP/1.1\r\nHost: $(uname -n)\r\nContent-type: text/html\r\nContent-length: 10\r\nConnection: Close\r\n\r\n"; 
-	size=$(echo -e $head |nc $c_url 80 2>&1|grep "Content-Length:"|awk -F"Content-Length: " '{print $2}'|tr -d "\r")
-	if [ "$size" == "" ]; then
-		size=0;
+	size=0;
+	if [[ ! "$PORT" == "443" ]]; then
+		head="HEAD $h_url HTTP/1.1\r\nHost: $(uname -n)\r\nContent-type: text/html\r\nContent-length: 10\r\nConnection: Close\r\n\r\n"; 
+		#echo "-- $h_url -- $c_url"
+		size=$(echo -e $head |nc $c_url $PORT 2>&1|grep "Content-Length:"|awk -F"Content-Length: " '{print $2}'|tr -d "\r")
+		if [ "$size" == "" ]; then
+			size=0;
+		fi
 	fi
-	echo "SESSION CONTENT OK: URL $h_url returned $pattern|time="$sum"ms;;;;0 size="$size"B;;;0"
+	echo "CONTENT_CHECK OK: URL $h_url returned $pattern|time="$sum"ms;;;;0 size="$size"B;;;0"
 	exit 0;
 fi
